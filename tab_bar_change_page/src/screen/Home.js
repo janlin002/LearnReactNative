db4676e857bcf68fe1656525469214d9e9bd80a7
janlin <123davidbill@gmail.com>
2021-07-16 13:43:40 +0800

    add renderList

--- tab_bar_change_page/src/screen/Home.js
+++ tab_bar_change_page/src/screen/Home.js
@@ -1,17 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export default function App(props) {
+  const [name, setName] = useState('jan');
+  const clickHandler = (msg) => {
+    setName(msg);
+  };
   return (
     <View style={styles.container}>
       <Text>Home</Text>
       <Button
         title="to Home Two"
-        onPress={() => props.navigation.push('HomeTwo')}
+        onPress={() =>
+          props.navigation.push('HomeTwo', {
+            name: 'jan',
+            functionA: (msg) => clickHandler(msg),
+          })
+        }
       />
+      <Text>{name}</Text>
     </View>
   );
 }
--- tab_bar_change_page/src/screen/HomeTwo.js
+++ tab_bar_change_page/src/screen/HomeTwo.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
+export default function App(props) {
   return (
     <View style={styles.container}>
       <Text>Home two</Text>
+      <Text>{props.route.params.name}</Text>
+      <Button
+        title="change first page"
+        onPress={() => props.route.params.functionA('bill')}
+      />
     </View>
   );
 }
